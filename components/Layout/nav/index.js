ff28c2c62a2424da8a2bc01d617da6930c3f7b67
devRabbani <dev.golamrabbani@gmail.com>
2022-12-08 01:08:53 +0530

    Adding search bar in nav

--- components/Layout/nav/index.js
+++ components/Layout/nav/index.js
@@ -2,18 +2,33 @@ import Link from 'next/link'
 import s from './nav.module.css'
 import { IoMdLogOut } from 'react-icons/io'
 import useLogout from '../../../hooks/useLogout'
+import { usePathname } from 'next/navigation'
+import { RiUserSearchFill, RiUserSearchLine } from 'react-icons/ri'
 
 export default function Nav({ uid }) {
   const { logout } = useLogout()
+  const path = usePathname()
   return (
     <nav>
-      <div className={`wrapper ${s.wrapper}`}>
-        <Link href="/">DailoSocial</Link>
-        {uid ? (
-          <div onClick={logout}>
-            Logout <IoMdLogOut />
+      <div
+        className={`wrapper ${s.wrapper} ${path === '/search' ? 'search' : ''}`}
+      >
+        {path === '/search' ? (
+          <div className={s.searchBar}>
+            <input autoFocus type="search" placeholder="Type to search user" />
+            {/* <RiUserSearchFill /> */}
+            <RiUserSearchLine />
           </div>
-        ) : null}
+        ) : (
+          <>
+            <Link href="/">DailoSocial</Link>
+            {uid ? (
+              <div className={s.logoutBtn} onClick={logout}>
+                Logout <IoMdLogOut />
+              </div>
+            ) : null}
+          </>
+        )}
       </div>
     </nav>
   )
